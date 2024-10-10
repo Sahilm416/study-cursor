6078f2ca9f17f77732cd4ab9aafadee3c55671ee
sahil <sahilmulani501@gmail.com>
2024-10-11 01:31:50 +0530

    add: initial routes

---
+++ app/dashboard/files/page.tsx
@@ -0,0 +1,12 @@
+import { NoFiles } from '@/components/dashboard/no-files'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen w-full'>
+      <NoFiles />
+    </div>
+  )
+}
+
+export default page
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import { Sidebar } from "@/components/dashboard";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="w-full h-full flex overflow-hidden">
+      <Sidebar />
+      {children}
+    </div>
+  );
+}
