67ec9f82ad12445eecbd9d4894c239ca05c16bb0
sahil <sahilmulani501@gmail.com>
2024-10-11 01:26:34 +0530

    refactor: next-auth route

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,6 @@
-import { NextAuthOptions } from "next-auth";
+import { authOptions } from "@/db/options";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-const CLIENT_ID = process.env.OAUTH_CLIENT_ID!;
-const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET!;
-
-const authOptions: NextAuthOptions = {
-  session: {
-    strategy: "jwt",
-  },
-  providers: [
-    GoogleProvider({
-      clientId: CLIENT_ID,
-      clientSecret: CLIENT_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ account, profile }) {
-      if (!profile?.email || !profile?.name) {
-        throw new Error("NO profile email");
-      }
-      return true;
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
