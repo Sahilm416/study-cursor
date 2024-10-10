993e10b8972100912ad782859e699ee2e6a9132b
sahil <sahilmulani501@gmail.com>
2024-10-11 01:25:15 +0530

    add: next-auth options

---
+++ db/options.ts
@@ -0,0 +1,36 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+const CLIENT_ID = process.env.OAUTH_CLIENT_ID!;
+const CLIENT_SECRET = process.env.OAUTH_CLIENT_SECRET!;
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: CLIENT_ID,
+      clientSecret: CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (!profile?.email || !profile?.name) {
+        throw new Error("NO profile email");
+      }
+
+      //   const db_res = await insertUser({
+      //     email: profile.email,
+      //     name: profile.name,
+      //     // @ts-ignore
+      //     profile_image: profile.picture,
+      //   });
+
+      //   if (db_res.success) {
+      //     return true;
+      //   }
+
+      return true;
+    },
+  },
+};
