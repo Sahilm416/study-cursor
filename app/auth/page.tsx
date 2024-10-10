f2da0548dcedf043987e3f8925b6b5d7cc05bf1a
sahil <sahilmulani501@gmail.com>
2024-10-11 01:40:24 +0530

    add: basic auth UI

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+"use client";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+import React from "react";
 
 const page = () => {
   return (
-    <div>
-        Continue with Google
+    <div className="flex justify-center items-center h-screen">
+      <Button onClick={() => signIn("google")}>Continue with Google</Button>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
