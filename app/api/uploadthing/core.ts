3436b976bf13b8f5028481b72012132795d7a9a3
sahil <sahilmulani501@gmail.com>
2024-10-11 16:51:06 +0530

    fix: cookies access on route

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -5,9 +5,7 @@ import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const supabase = createClient();
-
-const auth = async (req: Request) => await getUser();
+const auth = async (req: Request) => ({ userId: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -16,19 +14,17 @@ export const ourFileRouter = {
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
-      const session = await auth(req);
+      const user = await auth(req);
 
       // If you throw, the user will not be able to upload
-      if (!session.user) throw new UploadThingError("Unauthorized");
+      if (!user) throw new UploadThingError("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: session.user.email };
+      return { userId: user.userId };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
-
-
 
       console.log("file url", file.url);
 
