7e38284a529499f0aad79ea4a8ef2837ee36458f
sahil <sahilmulani501@gmail.com>
2024-10-11 16:41:02 +0530

    refactor: secure the file upload route

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,9 +1,13 @@
+import { getUser } from "@/actions/auth";
+import { createClient } from "@/db/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
 
-const auth = async (req: Request) => ({ id: "fakeId" }); // Fake auth function
+const supabase = createClient();
+
+const auth = async (req: Request) => await getUser();
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -12,17 +16,19 @@ export const ourFileRouter = {
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+      const session = await auth(req);
 
       // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
+      if (!session.user) throw new UploadThingError("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.id };
+      return { userId: session.user.email };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId);
+
+
 
       console.log("file url", file.url);
 
