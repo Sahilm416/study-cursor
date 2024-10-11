1b4006d32f5147fa4ae2210c252adac65d83166f
sahil <sahilmulani501@gmail.com>
2024-10-11 17:59:33 +0530

    add: post upload function

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,11 +1,10 @@
-import { getUser } from "@/actions/auth";
-import { createClient } from "@/db/server";
+import "server-only";
+import { uploadFile } from "@/actions/files";
+import { verifyUser } from "@/actions/verify";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
 const f = createUploadthing();
-
-const auth = async (req: Request) => ({ userId: "fakeId" }); // Fake auth function
 
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
@@ -14,22 +13,26 @@ export const ourFileRouter = {
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
-      const user = await auth(req);
+
+      const token = await verifyUser(req);
 
       // If you throw, the user will not be able to upload
-      if (!user) throw new UploadThingError("Unauthorized");
+      if (!token) throw new UploadThingError("Unauthorized");
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
-      return { userId: user.userId };
+      return { userId: token.email };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       // This code RUNS ON YOUR SERVER after upload
-      console.log("Upload complete for userId:", metadata.userId);
 
-      console.log("file url", file.url);
+      // if (error) throw new Error(error.message);
+      const res = await uploadFile({
+        userId: metadata.userId!,
+        url: file.url,
+        name: file.name,
+      });
 
-      // !!! Whatever is returned here is sent to the clientside `onClientUploadComplete` callback
-      return { uploadedBy: metadata.userId };
+      return res;
     }),
 } satisfies FileRouter;
 
