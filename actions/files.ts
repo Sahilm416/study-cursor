779929f380601f3a0f90994161683e9610628c64
sahil <sahilmulani501@gmail.com>
2024-10-12 09:48:39 +0530

    feat: delete files

--- actions/files.ts
+++ actions/files.ts
@@ -1,6 +1,7 @@
 "use server";
 import { supa } from "@/db/client";
 import { getUser } from "./auth";
+import { UTApi } from "uploadthing/server";
 
 type FileMetadata = {
   userId: string;
@@ -57,4 +58,40 @@ export const getCurrentUserFiles = async () => {
     message: "Fetched user files",
     files: data,
   };
+};
+
+export const deleteFile = async (file_key: string) => {
+  const res = await getUser();
+  if (!res.success || !res.user) {
+    return {
+      success: false,
+      message: "User not authenticated",
+    };
+  }
+
+  const utapi = new UTApi();
+
+  const { data, error } = await supa
+    .from("files")
+    .select("*")
+    .eq("file_key", file_key);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  if (data[0].uploaded_by !== res.user.email) {
+    throw new Error("You are not authorized to delete this file");
+  }
+
+  if (data.length === 0) {
+    throw new Error("File not found");
+  }
+
+  await supa.from("files").delete().eq("file_key", file_key);
+
+  const { deletedCount } = await utapi.deleteFiles([file_key]);
+  if (deletedCount === 0) {
+    throw new Error("Failed to delete file");
+  }
 };
