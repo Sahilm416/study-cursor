53a79ff65ccbf6abe55345d28fdc606cec0149d4
sahil <sahilmulani501@gmail.com>
2024-10-12 12:21:54 +0530

    add: doc viewer

--- app/dashboard/files/open/page.tsx
+++ app/dashboard/files/open/page.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { DocOpener } from "@/components/dashboard/doc-opener";
 
 const page = ({ searchParams }: { searchParams: { url: string } }) => {
   return (
-    <div>
-        <h1>{searchParams.url}</h1>
+    <div className="w-full min-h-screen">
+      <DocOpener url={searchParams.url} />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- components/dashboard/doc-opener.tsx
+++ components/dashboard/doc-opener.tsx
@@ -1,7 +1,28 @@
-export const DocOpener = () => {
+"use client";
+import DocViewer, {
+  DocViewerRenderers,
+  PDFRenderer,
+} from "@cyntler/react-doc-viewer";
+import "@cyntler/react-doc-viewer/dist/index.css";
+
+
+export const DocOpener = ({ url }: { url: string }) => {
   return (
-    <div>
-      <h1>DocOpener</h1>
+    <div className="w-full max-w-[900px] h-full overflow-y-auto max-h-screen relative">
+      
+      <DocViewer
+        config={{
+          header: {
+            disableFileName: true,
+            disableHeader: true,
+          },
+
+          pdfVerticalScrollByDefault: !false,
+        }}
+        className="selection:bg-yellow-500 selection:text-white relative"
+        documents={[{ uri: url }]}
+        pluginRenderers={[PDFRenderer]}
+      />
     </div>
   );
 };
