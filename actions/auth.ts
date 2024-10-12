f725157f12eb7fac70d5a6b44d786728d08c1252
sahil <sahilmulani501@gmail.com>
2024-10-12 10:40:00 +0530

    add : post signin to insert user

--- actions/auth.ts
+++ actions/auth.ts
@@ -1,6 +1,8 @@
 "use server";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/db/options";
+import { supa } from "@/db/client";
+import { v4 as uuidv4 } from "uuid";
 
 export async function getUser() {
   const session = await getServerSession(authOptions);
@@ -15,3 +17,15 @@ export async function getUser() {
     user: null,
   };
 }
+
+interface User {
+  email: string;
+  name: string;
+  profile_image: string;
+}
+
+export const insertUser = async (user: User) => {
+  const id = uuidv4();
+  const { data, error } = await supa.from("users").insert({ id, ...user });
+  return { success: true };
+};
