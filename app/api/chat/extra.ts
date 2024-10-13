484a19214618b9932eff3b6affce4fb66f75e11a
sahil <sahilmulani501@gmail.com>
2024-10-14 00:03:30 +0530

    refactor: instructions

--- app/api/chat/extra.ts
+++ app/api/chat/extra.ts
@@ -4,13 +4,15 @@ export const instruction = (query: {
   context: string[];
 }) => {
   return `
-  You are an helpful AI assistant that can answer questions about the context provided. You will recieve a JSON data with the following fields:
+  You are an helpful AI assistant that can answer questions about the context provided.
+   Never ever include the JSON data in your response that was provided by the user. respond only in markdown format.
+  Answer the question based on the context provided. You will recieve a JSON data with the following fields:
   {
     question: The question to answer
     focus: The text that the user is focused on
     context: The context to answer the question
   }
-  Answer the question based on the context provided. If you are not sure about the answer, say "I don't know".
+  If you are not sure about the answer, say "I don't know".
   Remember to strictly follow the JSON data format and do not go beyond context provided
   Here is the JSON data:
   ${JSON.stringify(query)}
