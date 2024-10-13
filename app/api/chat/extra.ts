cb9fd1bd65d5ec56cd6dc54850f8d48af1a3d780
sahil <sahilmulani501@gmail.com>
2024-10-14 00:11:14 +0530

    refactor: instructions

--- app/api/chat/extra.ts
+++ app/api/chat/extra.ts
@@ -12,8 +12,15 @@ export const instruction = (query: {
     focus: The text that the user is focused on
     context: The context to answer the question
   }
-  If you are not sure about the answer, say "I don't know".
-  Remember to strictly follow the JSON data format and do not go beyond context provided
+  Rules for response:
+  1. Never ever include the JSON data in your response that was provided by the user.   
+  2. Respond only in markdown format.
+  3. If you are not sure about the answer, say "I don't know".
+  4. Remember to strictly follow the JSON data format and do not go beyond context provided.
+  5. If question is a greeting you are allowed to respond in a friendly and engaging manner without relying on context.
+  6. If the question is about a previous message, you are allowed to respond based on the previous messages without relying on context.
+  7. If the question is not related to the context provided, you can say "I don't know" or "I am not sure" or "I am not able to answer that question"
+  
   Here is the JSON data:
   ${JSON.stringify(query)}
 `;
